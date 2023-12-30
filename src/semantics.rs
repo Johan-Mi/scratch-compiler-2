4faa9aa39dd94db6edf1cc0d77567b9fedb56d93
Johan-Mi <johanmi@protonmail.com>
2023-12-30 16:21:08 +0100

    Check top-level functions

--- src/semantics.rs
+++ src/semantics.rs
@@ -7,15 +7,27 @@ use crate::{
 pub fn check(document: &hir::Document, diagnostics: &mut Diagnostics) {
     for sprite in document.sprites.values() {
         for function in &sprite.functions {
-            check_function(function, diagnostics);
+            check_function(function, false, diagnostics);
         }
+    }
+    for function in &document.functions {
+        check_function(function, true, diagnostics);
     }
 }
 
-fn check_function(function: &hir::Function, diagnostics: &mut Diagnostics) {
+fn check_function(
+    function: &hir::Function,
+    is_top_level: bool,
+    diagnostics: &mut Diagnostics,
+) {
     match &**function.name {
         "when-flag-clicked" => {
-            if !function.parameters.is_empty()
+            if is_top_level {
+                diagnostics.error(
+                    "special function `when-flag-clicked` cannot be defined outside of a sprite",
+                    [primary(function.name.span, "")],
+                );
+            } else if !function.parameters.is_empty()
                 || function.return_ty.as_ref().is_ok_and(|it| *it != Ty::Unit)
             {
                 diagnostics.error(
