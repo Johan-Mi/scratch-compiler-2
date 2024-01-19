16c6f67f2ede9751bba270d750111bd28f692dde
Johan-Mi <johanmi@protonmail.com>
2024-01-19 11:23:52 +0100

    Canonicalize subtraction by constant into addition
    This might unlock some other optimizations later.

--- src/mir/optimization/canon.rs
+++ src/mir/optimization/canon.rs
@@ -1,9 +1,13 @@
-use crate::mir::{Op, Value::Imm};
+use crate::mir::{Imm::Num, Op, Value::Imm};
 
 pub(super) fn icalize(op: &mut Op) -> bool {
     if let Op::CallBuiltin { name, args, .. } = op {
         if matches!(&**name, "add" | "mul") && matches!(&**args, [Imm(_), _]) {
             args.reverse();
+            return true;
+        } else if let ("sub", [_, Imm(Num(n))]) = (&**name, &mut **args) {
+            *name = "add".to_owned();
+            *n *= -1.0;
             return true;
         }
     }
