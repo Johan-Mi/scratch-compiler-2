35f2d47f0b64bb88b6239cb0d9e20ed1af721616
Johan-Mi <johanmi@protonmail.com>
2024-01-20 13:58:46 +0100

    Don't canonicalize constant expressions

--- src/mir/optimization/canon.rs
+++ src/mir/optimization/canon.rs
@@ -1,11 +1,17 @@
-use crate::mir::{Imm::Num, Op, Value::Imm};
+use crate::mir::{
+    Imm::Num,
+    Op,
+    Value::{Imm, Var},
+};
 
 pub(super) fn icalize(op: &mut Op) -> bool {
     if let Op::CallBuiltin { name, args, .. } = op {
-        if matches!(&**name, "add" | "mul") && matches!(&**args, [Imm(_), _]) {
+        if matches!(&**name, "add" | "mul")
+            && matches!(&**args, [Imm(_), Var(_)])
+        {
             args.reverse();
             return true;
-        } else if let ("sub", [_, Imm(Num(n))]) = (&**name, &mut **args) {
+        } else if let ("sub", [Var(_), Imm(Num(n))]) = (&**name, &mut **args) {
             *name = "add".to_owned();
             *n *= -1.0;
             return true;
