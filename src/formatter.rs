6bd52db53f73f526aeccda5eaf99c30f2b09dbb0
Johan-Mi <johanmi@protonmail.com>
2024-02-11 18:59:45 +0100

    Make formatter recursive
    This is where things start to get interesting since we can now check the
    syntax kind of each node and format accordingly.

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,5 +1,9 @@
-use crate::diagnostics::Diagnostics;
+use crate::{
+    diagnostics::Diagnostics,
+    parser::{SyntaxNode, SyntaxToken},
+};
 use codemap::CodeMap;
+use rowan::NodeOrToken;
 
 pub fn format_stdin_to_stdout(diagnostics: &mut Diagnostics) -> Result<(), ()> {
     let source_code = std::io::read_to_string(std::io::stdin())
@@ -9,9 +13,37 @@ pub fn format_stdin_to_stdout(diagnostics: &mut Diagnostics) -> Result<(), ()> {
 }
 
 fn format(source_code: String) -> String {
+    let capacity = source_code.len();
     let file = CodeMap::new().add_file(String::new(), source_code);
     let cst = crate::parser::parse(&file, &mut Diagnostics::default());
+    let mut formatter = Formatter {
+        output: String::with_capacity(capacity),
+    };
+    formatter.node(&cst);
+    formatter.output
+}
 
-    // TODO: actually implement the formatter
-    cst.text().to_string()
+type SyntaxElement = rowan::SyntaxElement<crate::parser::Lang>;
+
+struct Formatter {
+    output: String,
+}
+
+impl Formatter {
+    fn node(&mut self, node: &SyntaxNode) {
+        for child in node.children_with_tokens() {
+            self.element(&child);
+        }
+    }
+
+    fn token(&mut self, token: &SyntaxToken) {
+        self.output.push_str(token.text());
+    }
+
+    fn element(&mut self, element: &SyntaxElement) {
+        match element {
+            NodeOrToken::Node(node) => self.node(node),
+            NodeOrToken::Token(token) => self.token(token),
+        }
+    }
 }
